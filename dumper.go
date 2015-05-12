d2da9dc550d0231b2000f29f02ad76835cec947a
Sergei Svistunov <sergei-svistunov@yandex.ru>
2015-05-13 00:31:25 +0300

    Initial commit

---
+++ dumper.go
@@ -0,0 +1,80 @@
+package dumper
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+)
+
+func Dump(w io.Writer, variable interface{}) {
+	(&dumper{
+		ptrs: make(map[uintptr]bool),
+	}).printReflect(w, reflect.ValueOf(variable))
+}
+
+type dumper struct {
+	ptrs map[uintptr]bool
+}
+
+func (d *dumper) printReflect(w io.Writer, reflectValue reflect.Value) {
+	switch reflectValue.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		fmt.Fprintf(w, "%s(%d)", reflectValue.Type().Name(), reflectValue.Int())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		fmt.Fprintf(w, "%s(%d)", reflectValue.Type().Name(), reflectValue.Uint())
+	case reflect.Bool:
+		fmt.Fprintf(w, "%t", reflectValue.Bool())
+	case reflect.String:
+		fmt.Fprintf(w, "%s(%q)", reflectValue.Type().Name(), reflectValue.String())
+	case reflect.Float32, reflect.Float64:
+		fmt.Fprintf(w, "%s(%f)", reflectValue.Type().Name(), reflectValue.Float())
+	case reflect.Ptr:
+		if reflectValue.IsNil() {
+			fmt.Fprintf(w, "%s(nil)", reflectValue.Type().Name())
+		} else {
+			addr := reflectValue.Pointer()
+			if _, exists := d.ptrs[addr]; exists {
+				fmt.Fprintf(w, "%s(&(0x%x))", reflectValue.Elem().Type().Name(), addr)
+			} else {
+				d.ptrs[addr] = true
+				fmt.Fprintf(w, "&(0x%x)", addr)
+				d.printReflect(w, reflectValue.Elem())
+			}
+		}
+	case reflect.Struct:
+		fmt.Fprintf(w, "%s{", reflectValue.Type().Name())
+		for i := 0; i < reflectValue.NumField(); i++ {
+			if i != 0 {
+				fmt.Fprint(w, ",")
+			}
+			field := reflectValue.Field(i)
+
+			fmt.Fprintf(w, "%s:", reflectValue.Type().Field(i).Name)
+			d.printReflect(w, field)
+		}
+		fmt.Fprint(w, "}")
+	case reflect.Array, reflect.Slice:
+		fmt.Fprintf(w, "%s{", reflectValue.Type().String())
+		for i := 0; i < reflectValue.Len(); i++ {
+			if i != 0 {
+				fmt.Fprint(w, ",")
+			}
+			d.printReflect(w, reflectValue.Index(i))
+		}
+		fmt.Fprint(w, "}")
+	case reflect.Map:
+		fmt.Fprintf(w, "%s{", reflectValue.Type().String())
+		keys := reflectValue.MapKeys()
+		for i, key := range keys {
+			if i != 0 {
+				fmt.Fprint(w, ",")
+			}
+			d.printReflect(w, key)
+			fmt.Fprint(w, ":")
+			d.printReflect(w, reflectValue.MapIndex(key))
+		}
+		fmt.Fprint(w, "}")
+	default:
+		fmt.Fprintf(w, "%s(???)", reflectValue.Kind().String())
+	}
+}
---
+++ dumper_test.go
@@ -0,0 +1,93 @@
+package dumper_test
+
+import (
+	"bytes"
+	"github.com/sergei-svistunov/dumper"
+	"regexp"
+	"testing"
+)
+
+var addrRe, _ = regexp.Compile("\\&\\(0x[0-9a-f]+\\)")
+
+type testStruct struct {
+	f1 int
+	f2 string
+	f3 *testStruct
+}
+
+func check(t *testing.T, v interface{}, expected, description string) {
+	buf := &bytes.Buffer{}
+	dumper.Dump(buf, v)
+
+	gotten := addrRe.ReplaceAllString(buf.String(), "&(0xADDR)")
+
+	if gotten != expected {
+		t.Errorf("%s: expected \"%s\", gotten \"%s\"", description, expected, gotten)
+	}
+}
+
+func TestDumpInt(t *testing.T) {
+	check(t, int(123), "int(123)", "Int")
+	check(t, int8(123), "int8(123)", "Int8")
+	check(t, int16(123), "int16(123)", "Int16")
+	check(t, int32(123), "int32(123)", "Int32")
+	check(t, int64(123), "int64(123)", "Int64")
+}
+
+func TestDumpUint(t *testing.T) {
+	check(t, uint(123), "uint(123)", "Uint")
+	check(t, uint8(123), "uint8(123)", "Uint8")
+	check(t, uint16(123), "uint16(123)", "Uint16")
+	check(t, uint32(123), "uint32(123)", "Uint32")
+	check(t, uint64(123), "uint64(123)", "Uint64")
+}
+
+func TestDumpBool(t *testing.T) {
+	check(t, true, "true", "Bool true")
+	check(t, false, "false", "Bool false")
+}
+
+func TestDumpFloat(t *testing.T) {
+	check(t, float32(10.5), "float32(10.500000)", "Float32")
+	check(t, float64(10.5), "float64(10.500000)", "Float64")
+}
+
+func TestDumpString(t *testing.T) {
+	check(t, string("Test\n,\t,\""), `string("Test\n,\t,\"")`, "String")
+}
+
+func TestDumpPtr(t *testing.T) {
+	intVar := int(123)
+	check(t, &intVar, "&(0xADDR)int(123)", "Ptr to int")
+	intVarPtr := &intVar
+	check(t, &intVarPtr, "&(0xADDR)&(0xADDR)int(123)", "Ptr to ptr to int")
+}
+
+func TestDumpStruct(t *testing.T) {
+	s := testStruct{
+		f1: 1,
+		f3: &testStruct{
+			f1: 2,
+			f3: &testStruct{
+				f1: 3,
+			},
+		},
+	}
+	check(t, s, `testStruct{f1:int(1),f2:string(""),f3:&(0xADDR)testStruct{f1:int(2),f2:string(""),f3:&(0xADDR)testStruct{f1:int(3),f2:string(""),f3:(nil)}}}`, "Struct")
+
+	s.f3.f3.f3 = &s
+	check(t, s, `testStruct{f1:int(1),f2:string(""),f3:&(0xADDR)testStruct{f1:int(2),f2:string(""),f3:&(0xADDR)testStruct{f1:int(3),f2:string(""),f3:&(0xADDR)testStruct{f1:int(1),f2:string(""),f3:testStruct(&(0xADDR))}}}}`, "Struct with cycle")
+}
+
+func TestDumpArray(t *testing.T) {
+	check(t, [3]int{1, 2, 3}, "[3]int{int(1),int(2),int(3)}", "Array")
+}
+
+func TestDumpSlice(t *testing.T) {
+	check(t, []int{1, 2, 3}, "[]int{int(1),int(2),int(3)}", "Slice")
+}
+
+func TestDumpMap(t *testing.T) {
+	t.Skip("Need keys sorting")
+	check(t, map[int]int{1: 10, 2: 20, 3: 30}, "map[int]int{int(1):int(10),int(2):int(20),int(3):int(30)}", "Map")
+}
